d677356dbfad3504b4e958b4eb364e879ec6c930
DeltaOmegas <gagarin20061@gmail.com>
2024-06-18 23:17:29 +0300

    Update camera_position.gd

--- Scripts/Player/Camera/camera_position.gd
+++ Scripts/Player/Camera/camera_position.gd
@@ -5,9 +5,9 @@ extends Marker2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	player.PlayerTurn.connect(_on_player_u_turn)
-	position.x = 30
+#func _ready():
+#	player.PlayerTurn.connect(_on_player_u_turn)
+#	position.x = 30
 
 
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,10 +15,10 @@ func _ready():
 #	pass
 
 
-func _on_player_u_turn(anim_direction):
+#func _on_player_u_turn(anim_direction):
 	#print("Player is turn to %s " % anim_direction)
-	match anim_direction:
-		"right":
-			position.x = 30
-		"left":
-			position.x = -30
+#	match anim_direction:
+#		"right":
+#			position.x = 30
+#		"left":
+#			position.x = -30
