454934a12c58c983b7e9bdd0f8c243d877d4ae54
DeltaOmegas <gagarin20061@gmail.com>
2024-06-21 21:57:54 +0300

    fixing neon's hands

--- Scripts/bed.gd
+++ Scripts/bed.gd
@@ -18,19 +18,14 @@ func _on_area_2d_body_exited(body):
 
 func _process(_delta):
 	var not_more_than_one_bed = %Player.beds_i_touch.len()<2#see if we are the only bed player touches, if not, dont allow him to harvest or to plant anything
-	if not_more_than_one_bed and player_in_the_area:
-		if plant and BackgroundScene.beds_list[self_pointer[0]][self_pointer[1]].ready_to_harvest:
-			player_in_the_area.harvest_hint_on()
-			if Input.is_action_just_pressed('action0'):
-				BackgroundScene.add_to_inventory('cabbage', 1) #TODO: add a more_than_one prob.
-				plant = false
-				$"..".remove_plant(self_pointer)
-		else:
-			player_in_the_area.plant_hint_on()
-			if Input.is_action_just_pressed('action0'):
-				if plant:
-					printerr('Planted two plants at once')
-				plant = $"..".create_plant('cabbage',self.position,self_pointer) #create plant and put it into our plant variable so we can know wich plant belongs to us
+	if not_more_than_one_bed and player_in_the_area and plant and BackgroundScene.beds_list[self_pointer[0]][self_pointer[1]].ready_to_harvest:
+		player_in_the_area.harvest_hint_on()
+	elif not_more_than_one_bed and player_in_the_area:
+		player_in_the_area.plant_hint_on()
+		if Input.is_action_just_pressed('action0'):
+			if plant:
+				printerr('Planted two plants at once')
+			plant = $"..".create_plant('cabbage',self.position,self_pointer) #create plant and put it into our plant variable so we can know wich plant belongs to us
 
 func _ready():
 	pass 
