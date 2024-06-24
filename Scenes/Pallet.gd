632f2b29d1267c0eaba37f6caf4bd467fe24852f
NeonX <mail.haller.daniel@gmail.com>
2024-06-24 12:24:38 +0300

    base for pallet UI

---
+++ Scenes/Pallet.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+
+
+func _on_body_entered(body):
+	if body is Player:
+		body.get_child(0).pallet_UI()
+	
+func _on_body_exited(body):
+	if body is Player:
+		body.get_child(0).main_UI()
