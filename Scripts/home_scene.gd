553093fb0bb339e8c42e903501cadeb72104dbae
DeltaOmegas <gagarin20061@gmail.com>
2024-06-29 01:11:59 +0300

    Added borders

---
+++ Scripts/home_scene.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_border_right_body_entered(body):
+	if body is Player:
+		if not BackgroundScene.current_scene_index == len(BackgroundScene.scenes_list)-1:#
+			BackgroundScene.current_scene_index += 1
+			get_tree().change_scene_to_file(BackgroundScene.scenes_list[BackgroundScene.current_scene_index])
