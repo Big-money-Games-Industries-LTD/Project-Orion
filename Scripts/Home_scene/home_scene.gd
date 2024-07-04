eccb109a621bfa06080d59fb3aadbbf2e9afd1e4
NeonX <mail.haller.daniel@gmail.com>
2024-07-04 10:23:46 +0200

    Textures

--- Scripts/Home_scene/home_scene.gd
+++ Scripts/Home_scene/home_scene.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
-
+func _process(_delta):
+	if Input.is_action_just_pressed("action0"):
+		BackgroundScene.current_scene_index = BackgroundScene.scene_saver
+		get_tree().change_scene_to_file(BackgroundScene.scenes_list[BackgroundScene.current_scene_index])
+		BackgroundScene.position_saver = 'right'
+		
 func _on_border_right_body_entered(body):
 	if body is Player:
 		BackgroundScene.current_scene_index = BackgroundScene.scene_saver
--- Scripts/Supply_scene/Load.gd
+++ Scripts/Supply_scene/Load.gd
@@ -1,17 +1,6 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-
 func _on_border_left_body_entered(body):
 	if body is Player:
 		BackgroundScene.current_scene_index = BackgroundScene.scene_saver
